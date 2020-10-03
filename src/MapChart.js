209518be49d38f4ae1d7306894049e3954065005
Artie <arturaszuta@gmail.com>
2020-10-03 17:30:01 -0400

    Marker test

--- src/MapChart.js
+++ src/MapChart.js
@@ -1,29 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ComposableMap,
   Geographies,
-  Geography
+  Geography,
+  ZoomableGroup,
+  Marker
 } from "react-simple-maps";
 
-const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-50m.json";
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-50m.json";
 
 const MapChart = () => {
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
+
+  function handleZoomIn() {
+    if (position.zoom >= 4) return;
+    setPosition(pos => ({ ...pos, zoom: pos.zoom * 2 }));
+  }
+
+  function handleZoomOut() {
+    if (position.zoom <= 1) return;
+    setPosition(pos => ({ ...pos, zoom: pos.zoom / 2 }));
+  }
+
+  function handleMoveEnd(position) {
+    setPosition(position);
+  }
+
   return (
-    <ComposableMap
-    //   projection="geoAzimuthalEqualArea"
-      
-    >
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies
-          .filter(d => d.properties.REGION_UN === "Americas" && 
-                       d.properties.SUBREGION === "Northern America" &&
-                       d.properties.NAME === "Canada")
-          .map(geo => <Geography key={geo.rsmKey} geography={geo} />)
-        }
-      </Geographies>
-    </ComposableMap>
+    <div>
+      <ComposableMap>
+        <ZoomableGroup
+          zoom={position.zoom}
+          center={position.coordinates}
+          onMoveEnd={handleMoveEnd}
+        >
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.filter(d => d.properties.REGION_UN === "Americas" && 
+              d.properties.SUBREGION === "Northern America" &&
+              d.properties.NAME === "Canada").map(geo => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
+            }
+          </Geographies>
+        <Marker coordinates={[-106.317,58.339 ]}>
+            <circle r={1} fill="#F53" />
+          </Marker>
+        </ZoomableGroup>
+      </ComposableMap>
+      <div className="controls">
+        <button onClick={handleZoomIn}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="12" y1="5" x2="12" y2="19" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+        <button onClick={handleZoomOut}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+      </div>
+    </div>
   );
 };
 
 export default MapChart;
+
+
