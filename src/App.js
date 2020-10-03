e204680c7e7f03ddc2089cb8e59c7247bec0ab56
a4vasude <aartidevan@gmail.com>
2020-10-03 14:03:34 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MapChart from './MapChart.js';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact>
-        {/* Place Component Here */}
+      </Route>
+      <Route path="/map-chart" exact>
+        <MapChart />
       </Route>
       {/* Add more Routes Here */}
     </Switch>
