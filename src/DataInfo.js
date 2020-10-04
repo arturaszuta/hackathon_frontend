28476a11e8dc8d1dc017d31b0472eda88d4f48cf
Emma Louise Houck <elhouck@uwaterloo.ca>
2020-10-04 17:54:02 -0400

    Datainfo updated

--- src/DataInfo.js
+++ src/DataInfo.js
@@ -5,19 +5,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import { CardHeader } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
-<<<<<<< HEAD
     headings: {
         alignItems: 'Center',
     },
     root: {
         margin: 8,
-        height: 600,
+        height: 700,
     },
     media: {
-      height: 50,
-      width: 170,
+      height: 42,
       paddingTop: '65.25%', // 16:9
       paddingLeft: '65%',
       spacing: 10,
@@ -26,21 +25,24 @@ const useStyles = makeStyles((theme) => ({
         color: 'darkseagreen',
         alignItems: 'Center',
     },
+    mediaExpress: {
+       height: 70,
+       paddingTop: 30,
+    },
   }));
-
 
 const DataInfo = () => {
     const classes = useStyles();
     return (
         <div>
-        <GridList cols={4} cellHeight={600} spacing={20} >
+        <GridList cols={4} cellHeight={700} spacing={20} >
             <GridListTile cols={1}>
                 <Card className={classes.root}>
                     <CardHeader
                         className={classes.subheadings}
-                        title="Datasets Used"
+                        title="Datasets"
                     />
-                    <GridList cols={1} cellHeight={200} spacing={4} >
+                    <GridList cols={1} cellHeight={300} spacing={4} >
                         <GridListTile cols={1}>
                             <Card className={classes.root}>
                                 <CardHeader
@@ -79,56 +81,46 @@ const DataInfo = () => {
                     <CardHeader
                         className={classes.headings}
                         title="How We Did This"
+                        subheader=" First, we visualized carbon monoxide emissions data from the 
+                        Government of Canada, listed by facility. We then used that location data to 
+                        filter the ODIAC CO2 fossil fuel emissions dataset to those Canadian locations, and
+                        plotted this small subset of the ODIAC data, allowing the user to compare emissions of the two gases.
+                        We took a similar approach for the emissions data in Sweden, using a list of cities with
+                        latitude and longitude values to pare down the ODIAC dataset, since facility emissions data 
+                        was not readily available. "
                     />
+                    <GridList cols={4} cellHeight={300} spacing={4} >
+                        <GridListTile cols={1}>
+                            <Card className={classes.root}>
+                                    <CardHeader
+                                        className={classes.headings}
+                                        subheader="React.js"
+                                    />
+                                    <CardMedia 
+                                    className={classes.media}
+                                    image={require('./logo192.png')}
+                                    title="odiac icon"
+                                    />
+                            </Card>
+                        </GridListTile>
+                        <GridListTile cols={1}>
+                            <Card className={classes.root}>
+                                    <CardHeader
+                                        className={classes.headings}
+                                        subheader=" Express.js"
+                                    />
+                                    <CardMedia 
+                                    className={classes.mediaExpress}
+                                    image={require('./expressLogo.jpg')}
+                                    title="odiac icon"
+                                    />
+                            </Card>
+                        </GridListTile>
+                    </GridList>
                 </Card>
             </GridListTile>
         </GridList>
         </div>
     );
-=======
-  headings: {
-    color: 'darkgrey',
-    alignItems: 'Center',
-  },
-  root: {
-    margin: 8,
-    height: 450,
-  },
-  media: {
-    height: 50,
-    width: 170,
-    paddingTop: '65.25%', // 16:9
-    paddingLeft: '35%',
-    spacing: 10,
-
-  },
-}));
-
-
-const DataInfo = () => {
-  const classes = useStyles();
-  return (
-    <div>
-      <GridList cols={4} cellHeight={400} spacing={20} >
-        <GridListTile cols={1}>
-          <Card className={classes.root}>
-            <CardMedia
-              className={classes.media}
-            />
-          </Card>
-        </GridListTile>
-        <GridListTile cols={3}>
-          <Card className={classes.root}>
-            <CardHeader
-              className={classes.headings}
-              title="How It's Made"
-              subheader="A detailed description of the dataset"
-            />
-          </Card>
-        </GridListTile>
-      </GridList>
-    </div>
-  );
->>>>>>> main
 };
 export default DataInfo;
