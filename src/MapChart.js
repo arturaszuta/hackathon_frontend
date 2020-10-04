d7192ffd9ec6f16a81f638bd02989c8d19a4f602
Artie <arturaszuta@gmail.com>
2020-10-04 17:01:11 -0400

    Add odiac dataset to the map

--- src/MapChart.js
+++ src/MapChart.js
@@ -9,6 +9,11 @@ import {
 } from "react-simple-maps";
 
 import useEmissions from "./hooks/useEmissions";
+import useOdiac from "./hooks/useOdiac";
+import Radio from "@material-ui/core/radio"
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import FormControlLabel from "@material-ui/core/FormControlLabel"
 
 const ComposableMap = styled(UCM)`
   max-height: calc(100vh - 170px);
@@ -21,42 +26,84 @@ const geoUrl =
 const MapChart = () => {
   const [position, setPosition] = useState({ coordinates: [-104.404259, 56.755051], zoom: 3 });
   const emissions = useEmissions();
+  const odiac = useOdiac();
+  const [filter, setFilter] = useState("emissions");
+
+  const GreenRadio = withStyles({
+    root: {
+      color: green[400],
+      '&$checked': {
+        color: green[600],
+      },
+    },
+    checked: {},
+  })((props) => <Radio color="default" {...props} />);
+  
+    const [selectedValue, setSelectedValue] = useState('emissions');
+  
+    const handleChange = (event) => {
+      setSelectedValue(event.target.value);
+    };
 
   function handleMoveEnd(position) {
     setPosition(position);
   }
 
-  function determineMarkerSize(emissionValue) {
-    if (emissionValue < 100) {
-      return 1;
-    } else if (emissionValue => 100 && emissionValue < 1000) {
+  function determineMarkerSize(emissionValue, type) {
+        if (emissionValue < 100) {
       return 2;
-    } else if (emissionValue >= 1000) {
+    } else if (emissionValue => 100 && emissionValue < 1000) {
       return 3;
+    } else if (emissionValue >= 1000) {
+      return 4;
     }
   }
 
-  function determineMarkerColour(emissionValue) {
-    if (emissionValue < 100) {
+  function determineMarkerColour(emissionValue, type) {
+    if (emissionValue < 100 && type == "emissions") {
       //return "#fad48e";
       //green
       return "#248f24";
-    } else if (emissionValue => 100 && emissionValue < 1000) {
+    } else if (emissionValue => 100 && emissionValue < 1000 && type == "emissions") {
       //yellow
       return "#c49743";
-    } else if (emissionValue >= 1000) {
+    } else if (emissionValue >= 1000 && type == "emissions") {
       //return "#9c6705";
       //red
       return "#ff1a1a";
+    } 
+
+   if (emissionValue < 100 && type == "odiac") {
+      //return "#fad48e";
+      //green
+      return "#248f24";
+    } else if (emissionValue => 100 && emissionValue < 1000 && type == "odiac") {
+      //yellow
+      return "#248f24";
+    } else if (emissionValue >= 1000 && type == "odiac") {
+      //return "#9c6705";
+      //red
+      return "#248f24";
+    }
+  }
+
+  function determineOpacity(emissionValue, type) {
+    if (emissionValue < 100) {
+      return 0.4;
+    } else if (emissionValue => 100 && emissionValue < 1000) {
+      return 0.6;
+    } else if (emissionValue >= 1000) {
+      return 0.8;
     }
   }
 
   function buildMarkers(dataArray, type) {
-    if (type === "emissions" && dataArray.length > 0) {
+    console.log("building markers with ", type, dataArray)
+    if (dataArray.length > 0) {
       return dataArray.map((e, keyAsIndex) => <Marker key={keyAsIndex} coordinates={[e.longitude, e.latitude]}>
-        <circle r={determineMarkerSize(e.emissions)}
-          fill={determineMarkerColour(e.emissions)}
-          fill-opacity="0.4" />
+        <circle r={determineMarkerSize(type == "emissions" ? e.emissions : e.odiak, type)}
+          fill={determineMarkerColour(type == "emissions" ? e.emissions : e.odiak, type)}
+          fillOpacity={determineOpacity(type == "emissions" ? e.emissions : e.odiak)}/>
       </Marker>)
     }
   }
@@ -67,20 +114,29 @@ const MapChart = () => {
     // <div id="body">
     <div>
     <div id="mapContainer">
-    <h1>
-        CO Emissions by Tonnes <br/> Canada | 2018
-    </h1>
-    {/* <svg>
-    <div id="body">
-      <div id="mapContainer">
-        <h1>
-          CO Emissions by Tonnes <br /> Canada | 2018
-        </h1>
-        {/* <svg>
-      <rect width="100" height="40" fill="#248f24" stroke-width="3" stroke="rgb(0,0,0)" x="30px" />
-      <rect width="100" height="40" fill="#c49743" stroke-width="3" stroke="rgb(0,0,0)" x="130px"/>
-      <rect width="100" height="40" fill="#ff1a1a" stroke-width="3" stroke="rgb(0,0,0)" x="230px"/>
-    </svg> */}
+    <div style={{ width: '500px', backgroundColor: 'whiteSmoke', padding: '20px'}}>
+    <FormControlLabel value="emissions" control={      <Radio
+        checked={selectedValue === 'emissions'}
+        onChange={handleChange}
+        value="emissions"
+        name="radio-button-demo"
+        inputProps={{ 'aria-label': 'A' }}
+      />} label="Canadian Facility CO Emissions - 2017" />
+      <FormControlLabel value="odiac" control={      <Radio
+        checked={selectedValue === 'odiac'}
+        onChange={handleChange}
+        value="odiac"
+        name="radio-button-demo"
+        inputProps={{ 'aria-label': 'B' }}
+      />} label="ODIAC CO2 Emissions - 2017" />
+
+    </div>
+    {selectedValue == "emissions" &&<h1>
+        CO Emissions by Tonnes - annual combined <br/> Canada | 2017
+    </h1>}
+    {selectedValue == "odiac" &&<h1>
+        CO2 Emissions by Tonnes - Monthly Total <br/> Canada | 2017
+    </h1>}
         <ComposableMap>
           <ZoomableGroup
             zoom={position.zoom}
@@ -96,7 +152,8 @@ const MapChart = () => {
                   ))
               }
             </Geographies>
-            {buildMarkers(emissions, "emissions")}
+            {selectedValue == "emissions" ? buildMarkers(emissions, "emissions") : ""}
+            {selectedValue == "odiac" ? buildMarkers(odiac, "odiac") : ""}
 
           </ZoomableGroup>
         </ComposableMap>
