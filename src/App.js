f545f6e3d5972955a67eb325bcc4c1a9aea8c71b
Artie <arturaszuta@gmail.com>
2020-10-04 15:11:12 -0400

    Finish UI Changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => (
       </Route>
       <Route path="/home">
         <MainContent />
+        <Home />
       </Route>
       <Route path="/AboutUs" >
         <AboutUs />
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import MainContent from './MainContent.js';
 import Header from './Header.js'
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from "@material-ui/core/CardContent"
+import Paper from "@material-ui/core/Paper"
+import { makeStyles } from '@material-ui/core/styles';
+import Grid  from '@material-ui/core/Grid';
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import Logo from './LOGO.png';
 
+const useStyles = makeStyles((theme) => ({
+    homeBox: {
+        width: '80%',
+        height: '600px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "center",
+        margin: '0 auto',
+        marginTop: '100px'
+    },
+    image: {
+        height: 500,
+        width: 450,
+    },
+    box: {
+        width: '500px',
+        height: '400px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '50px'
+    }
+
+  }));
 
 const Home = () => {
+    const classes = useStyles();
     return (
-        <div>
-        </div>
+        <Grid container alignItems="center">
+            <Box className={classes.homeBox}>
+                <Paper>
+                    <Box className={classes.box}>
+                        <img src={Logo} alt="website logo" className={classes.image}/>
+                        <Typography style={{marginBottom: "50px"}}>
+                            Greenhouse X
+                        </Typography>
+                        <Typography>
+                            Bringing Canadian CO2 and CO emissions visualization to your fingertips!
+                        </Typography>
+                    </Box>
+                   
+                </Paper>
+            </Box>
+        </Grid>
     );
 };
 export default Home;
